f084f2cf42b843fe6916a3ac9abfdb2e697f0c61
Robert Carlo <carlotubig@gmail.com>
2022-04-28 23:25:08 +0900

    header styling and search bar styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,21 @@ const Header = () => {
       <div className={HeaderCSS.nav}>
         <div className={HeaderCSS.nav_container}>
           <div className="nav-logo">LOGO</div>
-          <ul className="nav-links">
-            <li>link1</li>
-            <li>link2</li>
-            <li>link3</li>
+          <ul className={HeaderCSS.nav_links}>
+            <li>
+              <a href="#">link1</a>
+            </li>
+            <li>
+              <a href="#">link2</a>
+            </li>
+            <li>
+              <a href="#">link3</a>
+            </li>
           </ul>
         </div>
       </div>
       <main className={HeaderCSS.main}>
+        <p className={HeaderCSS.search_text}>Find A Recipe</p>
         <div className={HeaderCSS.search_bar}>
           <input
             type="text"
